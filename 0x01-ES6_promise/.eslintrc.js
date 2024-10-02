d907427ffd97dd798943fba2d8f54d17acd7897c
keziahNsoh <porepeya@gmail.com>
2024-10-02 14:22:19 +0000

    first push

---
+++ 0x01-ES6_promise/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+    'no-console': 'off',
+  },
+};
+
---
+++ 0x01-ES6_promise/README.md
@@ -0,0 +1,4 @@
+# ES6 Promises Project
+
+This project contains implementations of various tasks using ES6 Promises.
+
